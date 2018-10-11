3e12f0e822fc06f2bd7c0f19ad9938d34b0ac5db
tim <timothy.tran835@gmail.com>
2018-10-11 16:29:06 -0700

    Starting on chaining promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,20 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
+var chainedFiles = require('./promisification');
+var pluckFirstLineFromFile = require('./callbackReview');
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return chainedFiles.getGitHubProfileAsync(user)
+    .then(function (exisitingUser) {
+      if (exisitingUser) {
+        throw new Error (user + ' exists!');
+      } else {
+        return user; 
+      }
+    })
+    .then();
 };
 
 // Export these functions so we can test them
--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -13,7 +13,8 @@ var getGitHubProfile = function(user, callback) {
   var options = {
     url: 'https://api.github.com/users/' + user,
     headers: { 'User-Agent': 'request' },
-    json: true  // will JSON.parse(body) for us
+    json: true  
+    // will JSON.parse(body) for us
   };
 
   request.get(options, function(err, res, body) {
@@ -27,7 +28,7 @@ var getGitHubProfile = function(user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
 
 
 // (2) Asyncronous token generation
@@ -38,25 +39,25 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
 
 
 // (3) Asyncronous file manipulation
 var readFileAndMakeItFunny = function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, file) {
-    if (err) { return callback(err); }
-   
-    var funnyFile = file.split('\n')
-      .map(function(line) {
-        return line + ' lol';
-      })
-      .join('\n');
-
-    callback(funnyFile);
+    if (err) { 
+      callback(err); 
+    } else {
+      var funnyFile = file.split('\n')
+        .map(function(line) {
+          return line + ' lol';
+        }).join('\n');
+      callback(null, funnyFile);
+    }
   });
 };
 
-var readFileAndMakeItFunnyAsync; // TODO
+var readFileAndMakeItFunnyAsync = Promise.promisify(readFileAndMakeItFunny);
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
